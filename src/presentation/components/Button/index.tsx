117c77530538c6e3b5956209d6ed1787c6e9c540
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-01-22 17:46:59 +0300

    first commit

---
+++ src/presentation/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+import styles from "./Button.module.scss"
+
+const Button: FC = () => {
+    return (
+        <button className={styles.button}>
+            Button
+        </button>
+    )
+}
+
+export default Button
